354f70ed321305436d556bb9b4aa290828c85666
Fernando Hernandez <fernandoh@eventbrite.com>
2019-08-26 22:50:01 -0300

    tls v1_2

--- pysoa/common/transport/http2_gateway/core.py
+++ pysoa/common/transport/http2_gateway/core.py
@@ -227,7 +227,7 @@ class Http2ServerTransportCore(object):
 
     def receive_message(self, receive_timeout_in_seconds=None):
         try:
-            with self._get_timer('recieve.get_from_requests_queue'):
+            with self._get_timer('receive.get_from_requests_queue'):
                 stream_id, protocol_key, serialized_message = self.requests_queue.get(
                     timeout=receive_timeout_in_seconds or self.receive_timeout_in_seconds,
                 )
@@ -348,7 +348,7 @@ class Http2ClientTransportCore(object):
             self.secure = True
             self.ssl_context = pssl.create_default_context(pssl.Purpose.CLIENT_AUTH)
             self.ssl_context.options |= (
-                pssl.OP_NO_TLSv1 | pssl.OP_NO_TLSv1_1 | pssl.OP_NO_COMPRESSION
+                pssl.OP_NO_TLSv1 | pssl.OP_NO_TLSv1_2 | pssl.OP_NO_COMPRESSION
             )
             self.ssl_context.load_cert_chain(certfile="host.cert", keyfile="host.key")
             self.ssl_context.set_alpn_protocols(["h2"])
